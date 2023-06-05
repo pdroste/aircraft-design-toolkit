ab7853c9d304ab0151d1e2cec8811eb607b8c522
Paul Droste <droste.paul@gmx.de>
2023-06-05 12:21:16 +0200

    Add max_speed_boundary_prop.m
    Added function that returns a maximum power loading for a desired maximum air speed on a prop driven aircraft

---
+++ matlab/preliminary-sizing/max_speed_boundary_prop.m
@@ -0,0 +1,23 @@
+function [W2P] = ...
+    max_speed_boundary_prop( W2S, V_max, ref_density, design_density,...
+                        C_D0, K, eta_prop)
+%MAX_SPEED_BOUNDARY_PROP calculates the maximum power loading for the
+% desired maximum speed at design density.
+%   W2S:            array containing wing loading values to
+%                   calculate admissable power loading for [N/m^2]
+%   V_max:          desired maximum speed [m/s]
+%   ref_density:    reference density [kg/m^3]
+%   design_density: design density [kg/m^3]
+%   C_D0:           lift-independent drag coefficient [-]
+%   K:              induced drag coefficient 1/(pi*AR*e) [-]
+%   eta_prop:       propeller efficiency factor [-]
+
+density_ratio = design_density/ref_density;
+
+a = 0.5 * ref_density * C_D0;
+b = 2 * K / design_density / density_ratio;
+
+W2P = eta_prop ./ (a * V_max.^3 ./ W2S + b ./ V_max .* W2S);
+
+end
+

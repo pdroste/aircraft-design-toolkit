3fbf798c8ec23cbd0e5b59749e9606c8340d1c99
pdroste <droste.paul@gmx.de>
2023-11-26 21:43:49 +0100

    Add fourier_induced_drag_coefficient
    Add function to calculate the overall induced drag coefficient of the wing based on the uneven Fourier coefficients of the lift distribution

--- python/aerodynamics.py
+++ python/aerodynamics.py
@@ -39,8 +39,22 @@ def lifting_line(theta, c_l_alpha, chord, total_alpha, halfspan):
 def fourier_total_lift_coefficient(fourier_coefficients, aspect_ratio):
     """
     Return the total lift coefficient of the wing based on the first uneven Fourier coefficient.
-    :param fourier_coefficients:
-    :param aspect_ratio:
-    :return:
+    :param fourier_coefficients:    uneven Fourier coefficients of the lift distribution
+    :param aspect_ratio:            aspect ratio of the wing
+    :return:                        total lift coefficient of the wing
     """
     return np.pi * fourier_coefficients[0] * aspect_ratio
+
+def fourier_induced_drag_coefficient(fourier_coefficients, aspect_ratio):
+    """
+    Return the total induced drag coefficient of the wing based on the Fourier coefficients of the lift distribution.
+    :param fourier_coefficients:    uneven Fourier coefficients of the lift distribution
+    :param aspect_ratio:            aspect ratio of the wing
+    :return:                        total induced drag coefficient of the wing
+    """
+    #create uneven coefficient multiplier
+    n = np.arange(0, fourier_coefficients.size, 1) * 2 + 1
+
+    # return induced drag coefficient
+    return np.pi * aspect_ratio * np.sum(n * fourier_coefficients**2)
+
